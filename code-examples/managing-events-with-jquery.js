197a301ee795e8a0c021c1862842c6c4329b8ccd
Kevin Chisholm <kevinchisholm@Kevins-MBP.home>
2016-04-23 08:52:29 -0400

    add comments form submit handler code example

--- code-examples/managing-events-with-jquery.js
+++ code-examples/managing-events-with-jquery.js
@@ -222,30 +222,41 @@ overrideHyperlinkClicks();
 
 //Example # XX
 
+//click handler for the submit button
 $('#feedbackForm button[type="submit"]').click(function (event) {
+    //prevent default behavior
     event.preventDefault();
 
+    //hide the submit button
     $(this).hide();
 
+    //show the ajax spinner
     $('#ajaxSpinner').show();
 });
 //--------------------------------------------------------------------------------
 
 //Example # XX
 
+//click handler for the submit button
 $('#feedbackForm button[type="submit"]').click(function (event) {
+    //cache a reference to the element that was clicked
     var $me = $(this);
-    
-     event.preventDefault();
 
+    //prevent default behavior
+    event.preventDefault();
+
+    //hide the submit button
     $(this).hide();
 
+    //show the ajax spinner
     $('#ajaxSpinner').show();
   
+  //wait one second (mimic an ajax call)
   setTimeout(function (){
-    
+    //hide the ajax spinner
     $('#ajaxSpinner').hide();
-    
+
+    //show the submit button
     $me.show();
 
   }, 1000);
@@ -254,6 +265,96 @@ $('#feedbackForm button[type="submit"]').click(function (event) {
 //--------------------------------------------------------------------------------
 
 
+//Example # XX
+
+//empties all form fields
+function clearForm() {
+  //empty the name field
+  $('#name').val('');
+
+  //empty the comments field
+  $('#comments').val('');
+}
+
+//validates the form
+function formValid() {
+  var errorClassName = 'error',
+    errorFound = false,
+    $nameInput = $('#name'),
+    $commentsInput = $('#comments');
+
+  //if the name field is empty
+  if (!$nameInput.val()) {
+    //add the error class
+    $nameInput.addClass(errorClassName);
+
+    //set the flag that an error was found
+    errorFound = true;
+  }
+
+  //if the name field is empty
+  if (!$commentsInput.val()) {
+    //add the error class
+    $commentsInput.addClass(errorClassName);
+
+    //set the flag that an error was found
+    errorFound = true;
+  }
+
+  //if there was an error
+  if (errorFound) {
+    //return false
+    return false;
+  }
+
+  return true;
+}
+
+//click handler for the submit button
+$('#feedbackForm button[type="submit"]').click(function (event) {
+  //cache a reference to the element that was clicked
+  var $me = $(this);
+
+  //prevent default behavior
+  event.preventDefault();
+
+  //remove any previous error indicators
+  $('.error').removeClass('error');
+
+  //if the form has been completed
+  if (!formValid()) {
+    //inform the user that both form fields are reqeuired
+    alert('Please be sure to complete both the name and and comments fields.');
+
+    //stop here
+    return;
+  }
+
+  //hide the submit button
+  $(this).hide();
+
+  //show the ajax spinner
+  $('#ajaxSpinner').show();
+
+  //wait one second (mimic an ajax call)
+  setTimeout(function (){
+
+    //hide the ajax spinner
+    $('#ajaxSpinner').hide();
+
+    //show the submit button
+    $me.show();
+
+    //clear the form
+    clearForm();
+
+    //inform the user that thier comments have been submitted
+    alert('Your comments have been submitted. Thank you!');
+  }, 1000);
+});
+
+//ensure that all fields are empty on page load
+clearForm();
 //--------------------------------------------------------------------------------
 
 
